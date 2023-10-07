d537f62037f1eb04754996b1be3f4b78b7eed5d1
z <abdrashovv@gmail.com>
2023-10-07 22:23:23 +0600

    remove comments

--- message/message.php
+++ message/message.php
@@ -1,12 +1,5 @@
 <?php
 
-//return [
-//    'required' => ':field обязательно для заполнения.',
-//    'numeric' => ':field должно быть числом.',
-//    'string' => ':field должно быть строкой.',
-//    'phone' => ':field должно иметь формат +7 (***) ***-**-**.',
-//];
-
 return [
     'error' => [
         'message' => 'The given data was invalid.',
