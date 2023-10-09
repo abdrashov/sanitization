12eaecb5d79194e9512797d469c00fb191722b12
z <abdrashovv@gmail.com>
2023-10-09 22:17:08 +0600

    bug fix

--- src/Rule/Interface/RuleInterface.php
+++ src/Rule/Interface/RuleInterface.php
@@ -4,6 +4,8 @@ namespace Abdrashov\Sanitization\Rule\Interface;
 
 interface RuleInterface
 {
+    public function setValue(string $value = ''): void;
+
     public function type(): string;
 
     public function validate(): bool;
